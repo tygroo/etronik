35aff419acbb2c63bbb4dc917cd90ac67898930d
eti1boss <e.bossuet@gmail.com>
2014-05-26 14:08:31 +0200

    cherrypy+form [OK]

--- webApp/__init__.py
+++ webApp/__init__.py
@@ -1,6 +1,8 @@
 # coding=UTF-8
 # -*- coding: UTF-8 -*-
 import os
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
 
 import cherrypy
 from cherrypy.lib.static import serve_file
@@ -21,3 +23,8 @@ class Root:
     def index(self):
         template_index=env.get_template('index.html')
         return template_index.render()
+
+    @cherrypy.expose
+    def test(self, title, author, annee, query):
+    	a=title+" "+author+" "+annee+" "+query
+    	return a
