fdc14088926be78da0b673c3c8ed96d0712ead02
tygroo <tygroo972@gmail.com>
2014-06-13 14:46:28 +0200

    fabrique de notice xslt

--- webApp/createIndexes.sh
+++ webApp/createIndexes.sh
@@ -1,16 +1,19 @@
 createIndex(){
-	for ((i=$1; i<=$2; i++))
-	do
-		pdf2txt.py -p $i $3 >> $4
-	done		
+        for ((i=$1; i<=$2; i++))
+        do
+                pdf2txt.py -p $i $3 >> $4
+        done            
 }
 
 >index1
 >index2
 >index3
+>notice
+>noticetmp
 
 createIndex 1 1 $1 index1
 createIndex 2 8 $1 index2
 pdf2txt.py -m 9999 $1 >> index3
- 
-
+pdf2txt.py -p1 -t tag $1 >> noticetmp
+xsltproc --param name "'${1}'" notice.xslt noticetmp >> notice
+rm noticetmp
